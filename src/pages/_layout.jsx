bbdfbd243e141b4d3d2507f3a4d918e600205111
Aarvin Roshin <200008158+aarvinr@users.noreply.github.com>
2025-03-12 16:28:02 -0700

    minor: Metadata

--- src/pages/_layout.jsx
+++ src/pages/_layout.jsx
@@ -1,3 +1,10 @@
 export default function RootLayout({ children }) {
-	return children;
+	return (
+		<>
+			<meta charSet="utf-8" />
+			<meta name="viewport" content="initial-scale=1, width=device-width" />
+			<title>Yet Another URL Shortener</title>
+			{children}
+		</>
+	);
 }
