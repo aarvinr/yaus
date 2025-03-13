051bd842366b7277a833d17cd26941d436006ada
Aarvin Roshin <200008158+aarvinr@users.noreply.github.com>
2025-03-12 17:08:48 -0700

    major: basic UI

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,17 @@
 export default function HomePage() {
-	return <p>Hello, world!</p>;
+	return (
+		<main className="flex h-dvh flex-col items-center justify-center gap-3 bg-black text-white">
+			<h1 className="text-3xl font-bold">Yet Another URL Shortener</h1>
+
+			<form className="flex items-center gap-2">
+				<input
+					className="rounded border border-neutral-900 bg-neutral-950 p-1.5 transition-colors outline-none hover:border-neutral-800 focus:border-neutral-800"
+					type="url"
+				/>
+				<button className="rounded bg-white p-1.5 text-black" type="submit">
+					Shorten
+				</button>
+			</form>
+		</main>
+	);
 }
